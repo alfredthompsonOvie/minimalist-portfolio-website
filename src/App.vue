cf87af71a5bee30bf7f7e22de0e13c39c8f003d9
thompson <alfredthompsonovie@gmail.com>
2023-01-06 01:24:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+<RouterView />
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
